ca825e1b1b00d349f3def0889123c87fd2f18843
Dhanaraj N <dhanarajccs@gmail.com>
2023-02-07 12:02:06 +0530

    create GridTable component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import GridTable from "./components/GridTable";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Grid Tutorial</h1>
+      <GridTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GridTable.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GridTable = () => {
+  return (
+    <div>Grid Table</div>
+  )
+}
+
+export default GridTable
