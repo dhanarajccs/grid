bd7d3a4c8f8f32cbeef68092230490bdb0164b60
Dhanaraj N <dhanarajccs@gmail.com>
2023-02-07 12:43:23 +0530

    calling API using useEffect

--- src/components/GridTable.js
+++ src/components/GridTable.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const GridTable = () => {
-  return (
-    <div>Grid Table</div>
-  )
-}
 
-export default GridTable
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+  }, []);
+
+  return (
+    <>
+        Grid Table Tutorial
+    </>
+    )
+};
+
+export default GridTable;
+
