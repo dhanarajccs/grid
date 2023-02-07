fbfb87dac782df8788a7bc3b00456b141fdf90dc
Dhanaraj N <dhanarajccs@gmail.com>
2023-02-07 15:14:53 +0530

    adding fields (street, company) in DataGrid Table

--- src/components/GridTable.js
+++ src/components/GridTable.js
@@ -1,10 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
 
-import React, { useState, useEffect } from "react";
-import { DataGrid } from '@mui/x-data-grid'
+const columns = [
+  { field: 'id', headerName: 'ID' },
+  { field: 'username', headerName: 'User Name', width: 300 },
+  { field: 'email', headerName: 'Email', width: 300 },    
+  { 
+    field: 'street', headerName: 'Street', width: 300,
+    valueGetter: (tableData) => tableData.row.address.street
+  },
+  { 
+    field: 'company', headerName: 'Company', width: 300,
+    valueGetter: (tableData) => tableData.row.company.name
+  }
+];
 
 const GridTable = () => {
 
-  const [tableData, setTableData] = useState([])
+  const [tableData, setTableData] = useState([])  
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -14,26 +27,16 @@ const GridTable = () => {
 
   // console.log(tableData);
 
-  const columns = [
-    { field: 'id', headerName: 'ID' },
-    { field: 'username', headerName: 'User Name', width: 300 },
-    { field: 'email', headerName: 'Email', width: 300 },    
-    { 
-      field: 'street', headerName: 'Street', width: 300,
-      valueGetter: (tableData) => console.log(tableData)
-    },    
-  ];
-  
   return (
-    <>
-      <div style={{ height: 400, width: "100%" }}>
-        <DataGrid 
-          rows={tableData} 
-          columns={columns}
-        />
-      </div>
-    </>
-  )  
-};
+    <div style={{ height: 400, width: '100%' }}>
+      
+      <DataGrid 
+        rows={tableData} 
+        columns={columns}
+      />        
+    </div>
+  );
+}
 
-export default GridTable;
+export default GridTable
+
