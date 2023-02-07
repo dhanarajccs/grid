20b583d8c5d82317ec2c3f4542ddd829cb60a6e2
Dhanaraj N <dhanarajccs@gmail.com>
2023-02-07 16:01:43 +0530

    adding GridToolbar (Columns, Filters, Density, Export)

--- src/components/GridTable.js
+++ src/components/GridTable.js
@@ -1,69 +1,5 @@
-/*
-import React from 'react'
-
-const GridTable = () => {
-  return (
-    <div>Grid Table</div>
-  )
-}
-
-export default GridTable
-*/
-
-/*
-import React, { useState, useEffect } from "react";
-
-const GridTable = () => {
-  const [tableData, setTableData] = useState([]);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((json) => console.log(json))
-  }, []);
-
-  return (
-    <>
-        Grid Table Tutorial
-    </>
-    )
-};
-
-export default GridTable;
-*/
-
-
-
-/*
-import React, { useState, useEffect } from "react";
-import { DataGrid } from '@mui/x-data-grid'
-
-const GridTable = () => {
-
-  const [tableData, setTableData] = useState([])
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((data) => data.json())
-      .then((data) => console.log(data))
-  }, []);
-
-  console.log(tableData);
-
-  return (
-    <>
-      <div>
-        <DataGrid />
-      </div>
-    </>
-  )  
-};
-
-export default GridTable;
-*/
-
 import React, { useState, useEffect } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 
 const columns = [
   { field: 'id', headerName: 'ID' },
@@ -93,16 +29,21 @@ const GridTable = () => {
   // console.log(tableData);
 
   return (
-    <div style={{ height: 400, width: '100%' }}>      
+    <div style={{ height: 400, width: '100%' }}>
+      
       <DataGrid 
         rows={tableData} 
         columns={columns}
         pageSize={pageSize}
-        rowsPerPageOptions={[5, 10, 20]} 
         onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+        rowsPerPageOptions={[5, 10, 20]} 
+        components={{
+          Toolbar: GridToolbar,
+        }}
       />        
     </div>
   );
 }
 
 export default GridTable
+
